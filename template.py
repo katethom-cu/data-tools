e347579b79c698d902c665342450166c3d5575df
Will Thomas <thomaswj2@cardiff.ac.uk>
2025-06-17 13:32:18 +0100

    fixed template, removed __init__

--- template.py
+++ template.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import matplotlib.pyplot as plt
 from nptdms import TdmsFile
 from prefixed import Float
 import cavity_pert
@@ -7,7 +8,7 @@ import cavity_pert
 cr_001_a = TdmsFile.read("./CR-001-a.tdms")
 empty_sample = TdmsFile.read("./Empty Sample.tdms")
 
-results = cavity_pert.analyse(cr_001_a, empty_sample, CAVITY_SQUARED_RADII["Gandalf"], 0.001**2)
+results = cavity_pert.analyse(cr_001_a, empty_sample, cavity_pert.CAVITY_SQUARED_RADII["Gandalf"], 0.001**2)
 
 for name, data in results.items():
     data.plot(f"{name} = {Float(data.mean_f):.2h} Hz")
